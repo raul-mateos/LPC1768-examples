43a2001a3dfa7dfa6993db67698db0e420a898b5
Raul Mateos <raul.mateos@uah.es>
2024-10-28 10:56:05 +0100

    Fixing bugs in dac_adc_loopback project

--- dac_adc_loopback/main.c
+++ dac_adc_loopback/main.c
@@ -1,6 +1,9 @@
 #include <LPC17xx.h>
 #include <stdint.h>
 
+#define ADC_CLK_PIN
+
+
 #include "reg_masks.h"
 
 // Board features:
@@ -9,13 +12,14 @@
 
 void loopback_cfg(unsigned int tone_freq, unsigned int fs_adc);
 void loopback_enable(int enable);
-void key_cfg();
+
+#define TONE_FREQ     1000
+#define SAMPLE_FREQ   10000
 
 int main() {
 
-  loopback_cfg(1000, 16000);
-//  loopback_enable(1);
-  key_cfg();
+  loopback_cfg(TONE_FREQ, SAMPLE_FREQ);
+  loopback_enable(1);
 
   while(1);
 
@@ -24,9 +28,8 @@ int main() {
 
 #include <math.h>
 #define N_POINTS    16
-#define TONE_FREQ   5000
 
-#define DAC_N_BITS  10
+#define DAC_N_BITS    10
 #define DAC_N_LEVELS  (1U << DAC_N_BITS)
 #define DAC_MID_RANGE (1U << (DAC_N_BITS-1))
 
@@ -61,7 +64,7 @@ static void loopback_dac_cfg() {
   LPC_DAC->DACCTRL = 0;
 }
 
-// Timer 0 is used for the ADC
+// Timer 0 is used for the DAC
 
 static void loopback_dac_timer_cfg(int freq) {
   uint32_t mr;
@@ -92,14 +95,22 @@ void loopback_enable(int enable) {
 }
 
 
-// Timer 1 is used for the ADC
+// Timer 1 (MAT1.1) is used for the ADC
+
+#define MAT_FUNCTION  0x3
 
 static void loopback_adc_timer_cfg(unsigned int f_s) {
   uint32_t mr;
+  
+#ifdef ADC_CLK_PIN
+  LPC_PINCON->PINSEL3  |= (MAT_FUNCTION << 18); // P1.25 pin as MAT1.1 output
+  LPC_PINCON->PINMODE3 |= (PIN_NO_PULL << 18);  // P1.25: disable pull-up & down 
+#endif  
+  
   LPC_SC->PCONP |= PCTIM1;          // Timer 1 power On
   LPC_TIM1->PR = 0;                 // Set prescaler to 1
   LPC_TIM1->MCR = MR0R;             // clear timer on match
-  LPC_TIM1->EMR |= EM_TOGGLE << 4;  // MAT1.0 toggle on match
+  LPC_TIM1->EMR |= EM_TOGGLE << 6;  // MAT1.1 toggle on match
 
   mr = (uint32_t)((F_PCLK/(2*f_s))-1);
   LPC_TIM1->MR0 = mr;
@@ -130,110 +141,44 @@ static void loopback_adc_cfg() {
   LPC_ADC->ADCR = (1U << ADC_CHANNEL) |  // channel select
                   ADC_CLK_DIV(div-1)  |
                   ADC_NOT_PDN         |
-                  ADC_MAT1_0_START;    
+                  ADC_MAT1_1_START;    
 
   LPC_ADC->ADINTEN = ADGINTEN;
   NVIC_SetPriority(ADC_IRQn, 5);
   NVIC_EnableIRQ(ADC_IRQn);
 }
 
-static void adc_cfg_sw_trigger() {
-  uint32_t div;
-  
-  LPC_SC->PCLKSEL0  &= ~PCLK_ADC_MASK;
-  LPC_SC->PCLKSEL0  |= PCLK_ADC_DIV_4;  // PCLK_ADC = CCLK/4 (25 MHz)
-  
-  LPC_SC->PCONP |= PCADC;                       // ADC 0 power on
-  LPC_PINCON->PINSEL1  |= (ADC_FUNCTION << 18); // P0.25 pin as AD0.2 input
-  LPC_PINCON->PINMODE1 |= (PIN_NO_PULL << 18);  // P0.25: disable pull-up & down 
-  
-  div = DIV_ROUND_UP((uint32_t)F_PCLK, (uint32_t)FREQ_ADC_MAX);
+#define V_REF_P       3.3
+#define V_REF_N       0.0
+#define ADC_N_BITS    12
+#define ADC_N_LEVELS  (1U << ADC_N_BITS)
 
-  LPC_ADC->ADCR = (1U << ADC_CHANNEL) |  // channel select
-                  ADC_CLK_DIV(div-1)  |
-                  ADC_NO_START        |
-                  ADC_NOT_PDN;
-
-  LPC_ADC->ADINTEN = ADGINTEN;
-  NVIC_SetPriority(ADC_IRQn, 5);
-  NVIC_EnableIRQ(ADC_IRQn);  
-}
-
-
-
-
-// ADGINTEN = 1
-// La interrupción se dispara cuando el bit DONE de AD0GDR se activa.
-// El bit AD0GDR cuando concluye una conversión EN CUALQUIER CANAL
-//    => Esto NO se puede usar en modo burst! 
-// ADGINTEN = 0
-// La interrupción se dispara cuando para alguno de los canales cuyas
-// interrupciones se han habilitado individualmente en AD0INTEN se 
-// ha completado la conversión. Este hecho viene indicado por el bit
-// DONE del registro de datos de dicho canal AD0DRx
-//    => Esto SE DEBE usar en modo burst! 
-
-typedef struct {
-  unsigned              :  4;  // reserved
-  unsigned int result   : 12; // 
-  unsigned              :  8;  // reserved
-  unsigned int channel  :  3; // 
-  unsigned              :  3;  // reserved
-  unsigned int overrun  :  1; // 
-  unsigned int done     :  1; // 
-} ADGDR;
-
-int size = sizeof(ADGDR);
-
-int16_t sample;
-int channel;
-int ovr_flag, done_flag;
+// For debugging:
+uint16_t sample;
+float V_in;         // In Volts
 
 void ADC_IRQHandler() {
   uint32_t reg;
-  uint32_t status;
+  uint16_t adc_code;
+  int channel;
+  int ovr_flag, done_flag;
   
-  reg = sizeof(ADGDR);
   reg = LPC_ADC->ADGDR;
-  sample = ADC_GET_RESULT(reg);
+  adc_code = ADC_GET_RESULT(reg);
   channel = ADC_GET_CHANNEL(reg);
   ovr_flag = (reg & ADC_OVR)? 1 : 0;
   done_flag = (reg & ADC_DONE)? 1 : 0;
+  
+  sample = adc_code;
+  V_in = adc_code * ((V_REF_P-V_REF_N)/ADC_N_LEVELS) + V_REF_N;
 }
 
 void loopback_cfg(unsigned int tone_freq, unsigned int fs_adc) {
-//  loopback_init_samples();
-//  loopback_dac_cfg();
-//  loopback_dac_timer_cfg(tone_freq);
+  loopback_init_samples();
+  loopback_dac_cfg();
+  loopback_dac_timer_cfg(tone_freq);
 
   loopback_adc_cfg();
   loopback_adc_timer_cfg(fs_adc);  
-}
-
-// P2.10  => ISP
-// P2.11  => KEY1
-// P2.12  => KEY2
-// P2.13  => TP_IRQ
-
-#define EINT_FUNCTION   0x1
-
-void key_cfg() {
-  // Pushbutton configuration
-  LPC_PINCON->PINSEL4 |= (EINT_FUNCTION << 22); // P2.11 as EINT1
-  LPC_SC->EXTMODE = (0x1 << 1);         // edge sensitive...
-  LPC_SC->EXTPOLAR = 0;                 //...falling-edge sensitive
-  NVIC_SetPriority(EINT1_IRQn, 1);
-  NVIC_EnableIRQ(EINT1_IRQn);
-}
-
-// KEY1: Play
-void EINT1_IRQHandler() {
-  uint32_t reg;
-  LPC_SC->EXTINT = (0x1 << 1);    // Clear flag
-  
-  reg = LPC_ADC->ADCR;
-  reg &= ~ADC_START_MASK;
-  reg |= ADC_SW_START;
-  LPC_ADC->ADCR = reg;
 }
 
